3ccaac6a00a4b3f63e02b02b407f54356eb8450f
Scheufler, Henning <henning.scheufler@dlr.de>
2022-04-07 08:24:56 +0200

    added deform and Leveque for Tet

---
+++ benchmarksuite/InterfaceCapturing/advection/LevequeTet/genCases.py
@@ -0,0 +1,34 @@
+import casefoam
+
+case = [['isoAlpha', 'plicRDF','MULES'],
+        ['x16','x32', 'x64' ]] #, 'x128']]
+
+
+def updateScheme(advect,recon,sub_steps):
+        return {
+                'system/simulationParameter': {
+                    "ADVECTSCHEME": advect,
+                    "RECONSCHEME": recon,
+                    "SUBCYL": str(sub_steps)
+                }
+            }
+
+
+def changeGeoFile(Nx):
+    return {
+                'triBlock.geo': 
+                { '#!stringManipulation': 
+                    {
+                        "nx=32;": f"nx={Nx};"
+                    }
+                }
+            }
+
+data = {'isoAlpha': updateScheme("isoAdvection","isoAlpha",1),
+        'plicRDF': updateScheme("isoAdvection","plicRDF",1),
+        'MULES': updateScheme("MULESScheme","isoSurface",1),
+        'x16': changeGeoFile(16),
+        'x32': changeGeoFile(32),
+        'x64': changeGeoFile(64)}
+
+casefoam.mkCases('LevequeTet', case, data, 'tree', writeDir='Cases')
